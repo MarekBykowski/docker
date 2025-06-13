9de9e2a9ea11815c8a9965983944f60a86e36239
Marek Bykowski <marek.bykowski@gmail.com>
2025-06-13 07:50:38 -0700

    Docker eco-system: POC for it is up and running!
    Signed-off-by: Marek Bykowski <marek.bykowski@gmail.com>

--- dockerfile-yocto-ci/.bash_aliases
+++ dockerfile-yocto-ci/.bash_aliases
@@ -1,5 +1,5 @@
 cat << 'EOF'
-In docker you can run `Avery B2B` on the same machine.
-Source `enviroment_setup.sh` and folow the howto.
-`source ~/avery/2023_1215/avery_qemu-docker/enviroment_setup.sh`
+Welcome to the Docker yocto-ci continaer!
+
+This container run yocto-ci (auto-tests) from QEMU against either SV or COSIM.
 EOF
--- enviroment_setup.sh
+++ enviroment_setup.sh
@@ -1,9 +1,32 @@
 #!/bin/bash
 
-export D_UID=$(id -u) \
-   D_GID=$(id -g) \
-   D_PASSWORD=password \
-   D_USER=$USER \
-   D_HTTPS_PROXY=$HTTPS_PROXY \
-   D_HTTP_PROXY=$HTTP_PROXY \
-   D_HOME=$HOME
+# All of the values must be unique per user.
+# As the values are exported to the system and available for all the processes
+# I use prefix D_ for indicating this is for Docker containers.
+
+# Common values shared by two containers
+D_UID=$(id -u)
+D_GID=$(id -g)
+D_PASSWORD=password
+D_USER=$USER
+D_HTTPS_PROXY=$HTTPS_PROXY
+D_HTTP_PROXY=$HTTP_PROXY
+D_HOME=$HOME
+
+export D_UID D_GID D_PASSWORD D_USER D_HTTPS_PROXY D_HTTP_PROXY D_HOME
+
+# And some of the values must be unique per user and per container
+# Ports are defined from 'user id' + number which is system wise unique
+# and remains the same across logins.
+
+# Ports of container b2b
+D_B2B_SSH_PORT=$(($D_UID+10000)); echo "SSH PORT for b2b: $D_B2B_SSH_PORT"
+D_B2B_VNC_PORT=$(($D_UID+10001)); echo "VNC PORT for b2b: $D_B2B_VNC_PORT"
+D_B2B_NOVNC_PORT=$(($D_UID+10002)); echo "NOVNC PORT for b2b: $D_B2B_NOVNC_PORT"
+export D_B2B_SSH_PORT D_B2B_VNC_PORT D_B2B_NOVNC_PORT
+
+# Ports of container yocto-ci
+D_YOCTO_CI_SSH_PORT=$(($D_UID+20000)); echo "SSH PORT for yocto-ci: $D_YOCTO_CI_SSH_PORT"
+D_YOCTO_CI_VNC_PORT=$(($D_UID+20001)); echo "VNC PORT for yocto-ci: $D_YOCTO_CI_VNC_PORT"
+D_YOCTO_CI_NOVNC_PORT=$(($D_UID+20002)); echo "NOVNC PORT for yocto-ci: $D_YOCTO_CI_NOVNC_PORT"
+export D_YOCTO_CI_SSH_PORT D_YOCTO_CI_VNC_PORT D_YOCTO_CI_NOVNC_PORT
