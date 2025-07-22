63c3b200459c502e93977cf834a73a60c781881e
Marek Bykowski <marek.bykowski@gmail.com>
2025-07-22 09:22:15 +0000

    Update run.sh
    Signed-off-by: Marek Bykowski <marek.bykowski@gmail.com>

--- run.sh
+++ run.sh
@@ -1,33 +1,38 @@
 #!/bin/bash
 
+file=compose-armds-a55-a55arm32.yml
+echo -e "Running Docker compose ${file}\n"
+
 if [[ $1 == up ]]; then
-	docker compose -f compose-armds-a55-a55arm32.yml up -d
+	docker compose -f ${file} up -d
 elif [[ $1 == down ]]; then
-	docker compose -f compose-armds-a55-a55arm32.yml down --volumes
+	docker compose -f ${file} down --volumes
 elif [[ $1 == ps ]]; then
-	docker compose -f compose-armds-a55-a55arm32.yml ps
+	docker compose -f ${file} ps
 elif [[ $1 == config ]]; then
-	docker compose -f compose-armds-a55-a55arm32.yml config
+	docker compose -f ${file} config
+elif [[ $1 == vol ]]; then
+	docker volume ls
 elif [[ $1 == commit ]]; then
-	for t in `docker compose -f compose-armds-a55-a55arm32.yml config | grep container_name: | awk '{print $2}'`; do
+	for t in `docker compose -f ${file} config | grep container_name: | awk '{print $2}'`; do
 		c+=($t)
 	done
-	for t in `docker compose -f compose-armds-a55-a55arm32.yml config | grep image: | awk '{print $2}'`; do
+	for t in `docker compose -f ${file} config | grep image: | awk '{print $2}'`; do
 		i+=($t)
 	done
-	for t in `docker compose -f compose-armds-a55-a55arm32.yml ps --services`; do
+	for t in `docker compose -f ${file} ps --services`; do
 		service+=($t)
 	done
 	for ((t=0;t<3;t++)); do 
-		echo "docker compose -f compose-armds-a55-a55arm32.yml commit ${service[$t]} ${i[$t]}"
+		echo "docker compose -f ${file} commit ${service[$t]} ${i[$t]}"
 		#echo "docker commit ${c[$t]} ${i[$t]}"
 	done
 elif [[ $1 == push ]]; then
-	for image in `docker compose -f compose-armds-a55-a55arm32.yml config | grep image: | awk '{print $2}'`; do
+	for image in `docker compose -f ${file} config | grep image: | awk '{print $2}'`; do
 		echo "docker push $image"
 	done
 else
-	echo "$0 <up|down|ps|config|commit|push>"
+	echo "$0 <up|down|ps|config|commit|pushi|vol>"
 fi
 
 
