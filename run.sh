2e9122ac222306d08c1ddc44e1b3cd109aaf10d7
Marek Bykowski <marek.bykowski@gmail.com>
2026-01-17 12:48:47 -0500

    run.sh: demonize docker compose
    Signed-off-by: Marek Bykowski <marek.bykowski@gmail.com>

--- run.sh
+++ run.sh
@@ -13,8 +13,8 @@ if [[ $1 == up ]]; then
 	test -f docker-compose.log && rm -f docker-compose.log
 	# No cache does the pristine build even though the images are already build.
 	# Uncomment the line with "--no-cache" only when needed
-	#docker compose -f ${CF} build --no-cache --progress=plain | tee -a docker-compose.log
-	docker compose -f ${CF} up | tee -a docker-compose.log
+	docker compose -f ${CF} build --no-cache --progress=plain | tee -a docker-compose.log
+	docker compose -f ${CF} up -d | tee -a docker-compose.log
 	#docker compose --progress=plain -f ${CF} up | tee -a docker-compose.log
 	#docker compose --progress=plain -f ${CF} up --build | tee -a docker-compose.log
 	#COMPOSE_PROFILE=${ARGS[@]} docker compose --progress=plain -f ${CF} up --build | tee -a docker-compose.log
